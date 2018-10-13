bbc0ed5142f1283af1cd5ca8c07cd635b3bcea32
k2works <kakimomokuri@gmail.com>
2018-10-13 13:03:03 +0900

    開発 公開API作成

--- sam-app/fizz_buzz/app.py
+++ sam-app/fizz_buzz/app.py
@@ -1,27 +1,71 @@
 import json
 
-import requests
+from fizz_buzz import FizzBuzz
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
+def generate(event, context):
+    """FizzBuzz generate Lambda function
     Arguments:
         event LambdaEvent -- Lambda Event received from Invoke API
         context LambdaContext -- Lambda Context runtime methods and attributes
-
     Returns:
         dict -- {'statusCode': int, 'body': dict}
     """
+    try:
+        number = 0
 
-    ip = requests.get('http://checkip.amazonaws.com/')
+        if 'queryStringParameters' in event:
+            number = int(event['queryStringParameters']['number'])
 
+        body = json.dumps({
+            'value': FizzBuzz.generate(number)
+        })
+
+        print("Application execute with params:" + str(number))
+        return __create_response(200, body)
+    except Exception as err:
+        err_msg = 'Application error occurred:' + str(err.args)
+        print(err_msg)
+        body = json.dumps({
+            'message': err_msg
+        })
+        return __create_response(500, body)
+
+
+def iterate(event, context):
+    """FizzBuzz iterate Lambda function
+    Arguments:
+        event LambdaEvent -- Lambda Event received from Invoke API
+        context LambdaContext -- Lambda Context runtime methods and attributes
+    Returns:
+        dict -- {'statusCode': int, 'body': dict}
+    """
+    try:
+        params = json.loads(event['body'])
+        count = 0
+
+        if 'count' in params:
+            count = int(params['count'])
+
+        body = json.dumps({
+            'values': FizzBuzz.iterate(count)
+        })
+
+        print("Application execute with params:" + str(params))
+        return __create_response(200, body)
+    except Exception as err:
+        err_msg = 'Application error occurred:' + str(err.args)
+        print(err_msg)
+        body = json.dumps({
+            'message': err_msg
+        })
+        return __create_response(500, body)
+
+
+def __create_response(status_code, data):
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            'message': 'Hello Python lambda world',
-            'location': ip.text.replace('\n', ''),
-        }),
+        "statusCode": status_code,
+        "body": data,
         "headers": {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
