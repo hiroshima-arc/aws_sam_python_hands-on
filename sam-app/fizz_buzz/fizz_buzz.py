177838728b26b264c22039b80fb67a75e97580b5
k2works <kakimomokuri@gmail.com>
2018-10-13 10:58:32 +0900

    開発　条件を追加したのにテストが失敗

--- sam-app/fizz_buzz/fizz_buzz.py
+++ sam-app/fizz_buzz/fizz_buzz.py
@@ -7,6 +7,8 @@ class FizzBuzz:
             value = 'Fizz'
         elif value % 5 == 0:
             value = 'Buzz'
+        elif value % 3 == 0 and value % 5 == 0:
+            value = 'FizzBuzz'
 
         return value
     
